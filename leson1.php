1f6c428d7606f703b4e548c2495fb4e2eff15309
vic-mwenda <mwendav6@gmail.com>
2020-02-04 11:55:44 +0300

    intro to php

---
+++ leson1.php
@@ -0,0 +1,26 @@
+
+<?php
+//introduction to php
+//PHP is a widely used scripting language
+//PHP scripts are executed on the server
+//PHP files can contain text,HTML,CSS,javascript,and PHP code
+//PHP code is excecuted on the server and the result is returned to the browser as plain HTML
+//PHP files have extension ".php"
+
+echo 'hello world';
+echo "<h1>hello world</h1>";
+//echo:print content in browser
+//semicolon:means end of php statement or instruction
+//VARIABLES IN PHP
+//-A container for storing data
+$myname = "kamau fala";
+echo $myname;
+$mycompany = "microsoft";
+echo $mycompany;
+$company2 = "facebook";
+echo $company2;
+$company3 = "instagram";
+echo $company3;
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias culpa cupiditate delectus deleniti dolores explicabo fugit impedit laboriosam maiores, nemo nostrum obcaecati perspiciatis possimus quaerat quam quos reiciendis soluta voluptates!</p>
